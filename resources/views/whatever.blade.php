9d5eaaf686af179dd82935cac090e8875332aeec
Mhmdaris15 <muhammadaris1945@gmail.com>
2022-09-10 00:19:13 +0700

    Adminlte Installation - CRUD

---
+++ resources/views/whatever.blade.php
@@ -0,0 +1,11 @@
+@extends('adminlte::page')
+
+@section('title', 'Whatever')
+
+@section('content_header')
+<h1>Whatever</h1>
+@stop
+
+@section('content')
+<p>Welcome to this beautiful admin panel.</p>
+@stop
